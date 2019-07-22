3de4918e6bc99b259e091ce0980212e28a0245d0
ZYHowell <1148830469@qq.com>
2019-07-22 11:30:16 +0800

    ganimation add to(device)

--- GANimation/train.py
+++ GANimation/train.py
@@ -11,30 +11,34 @@ def grad_loss(matrix):
     return torch.sum(matrix[:, :, :, :-1] - matrix[:, :, :, 1:]) + \
     torch.sum(matrix[:, :, :-1, :] - matrix[:, :, 1:, :])
 
-if __name__ == "__main__":
+def train(device):
     G = m.generator()       # color, attention
     D = m.discriminator()   # I, y
+    G = G.to(device)
+    D = D.to(device)
 
     G_optim = optim.Adam(G.parameters(), lr=0.0002)
     D_optim = optim.Adam(D.parameters(), lr=0.0002)
 
-    MSE_criterion = nn.MSELoss()
-    L1_criterion = nn.L1Loss()
+    MSE_criterion = nn.MSELoss().to(device)
+    L1_criterion = nn.L1Loss().to(device)
     """dataset there"""
     dataset = []
 
     for _ in range(epoch):
         for i, images, tags in enumerate(dataset):
+            images = images.to(device)
+            tags = tags.to(device)
             perm = torch.randperm(batch_size)
             f_tags = tags[perm]
 
             A_in, C_in = G(images, f_tags)
-            full_in = torch.tensor(torch.ones_like(A_in))
+            full_in = torch.ones_like(A_in, device=device)
             f_images = (full_in - A_in).mul(C_in) + A_in.mul(images)
             """or, instead of mul, it is mm? since it simply uses operator*"""
 
             A_f, C_f = G(f_images)
-            full_f = torch.tensor(torch.ones_like(A_f))
+            full_f = torch.ones_like(A_f)
             back_images = (full_f - A_f).mul(C_f) + A_in.mul(f_images)
 
             DIg, DYg = D(f_images)
@@ -56,6 +60,6 @@ if __name__ == "__main__":
             """maybe a const to modify the training penalty of G is needed there"""
 
             if i % 1000 == 0:
-                torch.save(G.state_dict(), "gen.pth")
-                torch.save(D.state_dict(), "dis.pth")
+                torch.save(G.state_dict().cpu(), "gen.pth")
+                torch.save(D.state_dict().cpu(), "dis.pth")
                 # and output the loss now there?
